0b7ccc55881ade51844537f88a00422afe426e15
mgpauly1548 <mgpauly@hawaii.edu>
2023-04-16 16:01:54 -1000

    switch_util.c added, switch_main only remains in switch.c

--- src/switch.c
+++ src/switch.c
@@ -14,87 +14,6 @@
 #include "switch.h"
 #include "sockets.h"
 
-#define MAX_FILE_BUFFER 1000
-#define MAX_MSG_LENGTH 100
-#define MAX_DIR_NAME 100
-#define MAX_FILE_NAME 100
-#define PKT_PAYLOAD_MAX 100
-#define TENMILLISEC 10000    //10 millisecond sleep
-
-
-/* Job queue operations */
-struct host_job *job_q_remove(struct job_queue *j_q);
-void job_q_init(struct job_queue *j_q);
-int job_q_num(struct job_queue *j_q);
-
-void display_port_info(struct net_port *p) {
-    printf("Net port:\n");
-    printf("  type: %d\n", p->type);
-    printf("  pipe_host_id: %d\n", p->pipe_host_id);
-    printf("  pipe_send_fd: %d\n", p->pipe_send_fd);
-    printf("  pipe_recv_fd: %d\n", p->pipe_recv_fd);
-    printf("  next: %p\n", (void *) p->next);
-    printf("  sock_host_id: %d\n", p->sock_host_id);
-}
-
-void switch_job_q_add(struct switch_job_queue *j_q, struct switch_job *j)
-{
-if (j_q->head == NULL ) {
-	j_q->head = j;
-	j_q->tail = j;
-	j_q->occ = 1;
-}
-else {
-	(j_q->tail)->next = j;
-	j->next = NULL;
-	j_q->tail = j;
-	j_q->occ++;
-}
-}
-
-void display_forward_table(struct forward_table table) {
-   int i;
-   printf("Forward table:\n");
-   printf("Size: %d\n", table.size);
-   printf("Valid\tHost ID\tPort\n");
-   for (i = 0; i < 100; i++) {
-      if (table.valid[i] != 0 || table.HostID[i] != -1) {
-         printf("%d\t%d\t%d\n", table.valid[i], table.HostID[i], table.port[i]);
-      }
-   }
-}
-
-
-void init_forward_table(struct forward_table *table) {
-   int i;
-   table->size = 0;
-   for (i = 0; i < MAX_TABLE_SIZE; i++) {
-      table->valid[i] = 0;
-      table->HostID[i] = -1;
-      table->port[i] = 0;
-   }
-}
-
-void add_src_to_table(struct forward_table *table, struct packet *pkt, int port_index) {
-   if (table->valid[(int)pkt->src]) return;
-   table->valid[(int)pkt->src] = 1;
-   table->HostID[(int)pkt->src] = pkt->src;
-   table->port[(int)pkt->src] = port_index;
-
-}
-
-void send_to_all_ports(int node_port_num, struct net_port **node_port, struct packet *pkt) {
-   for (int k = 0; k < node_port_num; k++) {
-      packet_send(node_port[k], pkt);
-   }
-  free(pkt);
-}
-
-
-int is_host_in_table(struct forward_table *table, char dst) {
-  // printf("\ndst=%d table->valid[(intt)dst]=%d\n", dst, table->valid[(int)dst]);
-   return table->valid[(int)dst];
-}
 
 void switch_main(int host_id) {
 
@@ -105,8 +24,6 @@ void switch_main(int host_id) {
    struct packet *in_packet;
    struct packet *new_packet;
    struct net_port *p;
-   struct job_queue job_q;
-   struct switch_job *new_job;
    int i, k, n;
    struct forward_table table;
 
@@ -129,7 +46,6 @@ void switch_main(int host_id) {
    
    }
 
-   job_q_init(&job_q);
 
   // display_forward_table(table);
 
