7323b3092becba9f82664255131473f56c3e219f
mgpauly1548 <mgpauly@hawaii.edu>
2023-04-27 17:09:49 -1000

    removed un-necessary structs form switch.h

--- src/switch.h
+++ src/switch.h
@@ -2,19 +2,6 @@
 
 void switch_main(int);
 
-struct switch_job {
-   struct packet *packet;
-   int in_port_index;
-   int out_port_index;
-   struct switch_job *next;
-};
-
-struct switch_job_queue {
-   struct switch_job *head;
-   struct switch_job *tail;
-   int occ;
-};
-
 void display_port_info(struct net_port*);
 
 struct forward_table {
